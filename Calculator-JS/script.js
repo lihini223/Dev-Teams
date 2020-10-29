5d2e1d3c5e39008fafc3fab68f12e176e2f17f89
SeneheB <59204667+SeneheB@users.noreply.github.com>
2020-10-29 22:40:49 +0530

    Added key presses

--- Calculator-JS/script.js
+++ Calculator-JS/script.js
@@ -1,1 +1,23 @@
-
+function dis(val) {
+  document.getElementById("result").value += val;
+}
+/*
+function clr() 
+         { 
+             document.getElementById("result").value = "" 
+         } 
+function perc()
+{
+document.getElementById("result").value=result/100
+}
+function power()
+{
+    document.getElementById("result").value=result*result
+}
+function solve() 
+         { 
+             let x = document.getElementById("result").value 
+             let y = eval(x) 
+             document.getElementById("result").value = y 
+         } 
+         */
