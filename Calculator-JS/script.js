3860b77b48d9b44c18f50e574a637c4672567c3a
KHSNSugeeshwara <61137160+KHSNSugeeshwara@users.noreply.github.com>
2020-10-30 21:00:44 +0530

    add insert and equal functions

--- Calculator-JS/script.js
+++ Calculator-JS/script.js
@@ -1,1 +1,9 @@
-
+function insert(num){
+    document.form.textview.value=document.form.textview.value+num;
+}
+function equal(){
+    var exp = document.form.textview.value
+    if(exp){
+        document.form.textview.value=eval(exp);
+    }
+}
